8c091f8c4e4ebd46b77d81f2824c034552833a6b
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-04-04 22:47:08 +0500

    testing API working or not

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState , useEffect } from 'react';
+import { fetchDataFromApi } from './utils/api'
+
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  
+  useEffect(() => {
+    apiTesting();
+  },[]);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular")
+    .then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    })
+  }
+
+  return (
+    <div className="App">
+      {url?.total_pages}
+    </div>
+  )
+}
+
+export default App;
