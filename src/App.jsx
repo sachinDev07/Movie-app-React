7a5316e680a2bb2adbf9d86ac2cdc2218ec0a4fd
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-04-06 23:53:52 +0500

    some changes and added functionalities in fetchApiConfig

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,20 @@ const App = () => {
   const { url } = useSelector((state) => state.home);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      
+      dispatch(getApiConfiguration(url));
     });
   };
 
