eb5b15846cea32c1764217c802af4af61418f575
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-04-15 20:58:04 +0500

    corousel section done with slider

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -13,25 +13,38 @@ import PosterFallback from "../../assets/no-poster.png";
 
 import "./carousel.scss";
 import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 const Carousel = ({ data, loading }) => {
   const carouselContainer = useRef();
   const { url } = useSelector((state) => state.home);
   const navigate = useNavigate();
 
-  const navigation = (dir) => {};
+  const navigation = (dir) => {
+    const container = carouselContainer.current;
+
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+
+    container.scrollTo({
+      left : scrollAmount,
+      behavior : "smooth",
+    });
+  };
 
   const skItem = () => {
     return (
-        <div className="skeletonItem">
-            <div className="posterBlock skeleton"></div>
-            <div className="textBlock">
-                <div className="title skeleton"></div>
-                <div className="date skeleton"></div>
-            </div>
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
         </div>
-    )
-  }
+      </div>
+    );
+  };
 
   return (
     <div className="carousel">
@@ -45,7 +58,7 @@ const Carousel = ({ data, loading }) => {
           onClick={() => navigation("right")}
         />
         {!loading ? (
-          <div className="carouselItems">
+          <div className="carouselItems" ref={carouselContainer}>
             {data?.map((item) => {
               const posterUrl = item.poster_path
                 ? url.poster + item.poster_path
@@ -55,13 +68,12 @@ const Carousel = ({ data, loading }) => {
                   <div className="posterBlock">
                     <Img src={posterUrl} />
                     <CircleRating rating={item.vote_average.toFixed(1)} />
+                    <Genres id={item.genre_ids.slice(0, 2)} />
                   </div>
                   <div className="textBlock">
-                    <span className="title">
-                        {item?.title || item?.name}
-                    </span>
+                    <span className="title">{item?.title || item?.name}</span>
                     <span className="date">
-                        {dayjs(item.release_date).format("MMM D, YYYY")}
+                      {dayjs(item.release_date).format("MMM D, YYYY")}
                     </span>
                   </div>
                 </div>
