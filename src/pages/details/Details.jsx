7df217903852bf8f94fe2b289bb081262d8af574
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-04-18 21:38:56 +0500

    done Detail page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import DetailBanner from "./detailBanner/DetailBanner";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+
   return (
     <div className="detailPage">
-      <DetailBanner />
+      <DetailBanner video={data?.results?.[0]} crew={credits?.crew} />
     </div>
   );
 };
