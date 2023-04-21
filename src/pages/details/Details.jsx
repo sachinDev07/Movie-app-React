9d454baf0a280f92f1b6b0775dfd842d107e43ea
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-04-21 21:41:19 +0500

    imported Similar and Recommedation component

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Cast from "./cast/Cast";
 import VideosSection from "./videoSection/VideoSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
   const { mediaType, id } = useParams();
@@ -17,6 +19,8 @@ const Details = () => {
       <DetailBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 };
